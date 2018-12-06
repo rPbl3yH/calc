ecc2178549559bc2967d825fe78b2437452e7f45
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2018-12-06 18:28:52 +0500

    Update Сортировка.py

--- Сортировка.py
+++ Сортировка.py
@@ -1,15 +1,14 @@
-
 def sortirovka(n):
     n=n.split()
     s=len(n)
-    for i in range(0,s-1):
-        for j in range(0,s-i-1):
-            if n[j+1]<n[j]:
-                k=n[j]
+    for i in range(s-1):
+        for j in range(s-i-1):
+            if int(n[j+1])<int(n[j]):
+                l=n[j]
                 n[j]=n[j+1]
-                n[j+1]=k
+                n[j+1]=l
     for i in range(s):
         print(n[i])
-    return n,s,k
+    return n,s,l
 k=input()
 sortirovka(k)
