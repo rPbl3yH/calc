3f14c9b6ae9e7f42bd9b255ca7eb034aadbea122
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2019-02-04 18:52:43 +0500

    Update Алгоритм Цезаря.py

--- Алгоритм Цезаря.py
+++ Алгоритм Цезаря.py
@@ -1,19 +1,18 @@
 from tkinter import *
-alfavit = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 root = Tk()
-s1 = ''
 def cifer():
-    global s1
-    s1.replace(s1,'')
-    s2 = str(ent.get())
+    global s2
+    s2 = ''
+    s2 = list(ent.get().lower())
     for i in range(0,len(s2)):
-        if s2[i] == alfavit[-1]:
-            s1 += alfavit[0]
-        else:
-            for g in range(0,len(alfavit)):
-                if s2[i] == alfavit[g]:
-                    s1 += alfavit[g+1]
-    ext2['text'] = s1
+        for j in range(97,123):
+            if j == 122:
+	            ord(s2[i]) = 97 
+	    elif ord(s2[i]) == j:
+            ord(s2[i]) = j+1
+	        break 
+    s2.join
+    ext2['text'] = s2
 ext = Label(text = 'Введите слово для шифрования/дешифрования')
 ent = Entry()
 ext2 = Label()
@@ -22,6 +21,4 @@ ext.pack()
 ent.pack()
 ext2.pack()
 btn.pack()
-#ent2 = Entry()
-#ent2.pack()
 root.mainloop()
