e9bd51ac2d8bc10e922cf3eaaeb23bd4132c62dc
rPbl3yH <44815308+rPbl3yH@users.noreply.github.com>
2019-02-09 18:49:54 +0500

    Update Алгоритм Цезаря.py

--- Алгоритм Цезаря.py
+++ Алгоритм Цезаря.py
@@ -1,51 +1,43 @@
 from tkinter import *
 root = Tk()
-def cifer():
-    global s2
-    s2 = ''
-    s2 = list(ent.get().lower())
-    for i in range(0,len(s2)):
+def cifer_one():
+    global s1
+    s1 = ''
+    s1 = list(ent.get().lower())
+    for i in range(0,len(s1)):
         for j in range(97,123):
             if j == 122:
-	            ord(s2[i]) = 97 
-	    elif ord(s2[i]) == j:
-            ord(s2[i]) = j+1
-	        break 
-    s2.join
-    ext2['text'] = s2
-ext = Label(text = 'Введите слово для шифрования/дешифрования')
+                s1[i] = chr(97)
+            elif ord(s1[i]) == j:
+                s1[i] = chr(j+1)
+                break
+    ext2['text'] = ''.join(s1)
+def cifer_two():
+    global s2
+    s2 = ''
+    s2 = list(ent2.get().lower())
+    for h in range(0,len(s2)):
+        for g in range(97,123):
+            if g == 97:
+                s2[h] = chr(122)
+            elif ord(s2[h]) == g:
+                s2[h] = chr(g-1)
+                break
+    ext4['text'] = ''.join(s2)
+ext = Label(text = 'Введите слово для шифрования')
 ent = Entry()
 ext2 = Label()
-btn = Button(text = 'Расшифровать!',command = cifer)
+btn = Button(text = 'Расшифровать!',command = cifer_one)
 ext.pack()
 ent.pack()
 ext2.pack()
 btn.pack()
+ext3 = Label(text = 'Введите слово для дешифрования')
+ent2 = Entry()
+ext4 = Label()
+btn2 = Button(text = 'Расшифровать!',command = cifer_two)
+ext3.pack()
+ent2.pack()
+ext4.pack()
+btn2.pack()
 root.mainloop()
-
-
-#from tkinter import *
-#root = Tk()
-def cifer(s2):
-    #global s2
-    s2 = ''
-    s2 = s2.lower().split()
-    for i in range(0,len(s2)):
-        for j in range(97,123):
-            if j == 122:
-                s2[i] = chr(97)
-            elif ord(s2[i]) == j:
-                s2[i] = chr(j+1)
-                break 
-    s2.join()
-    ext2['text'] = s2
-cifer(input())
-#ext = Label(text = 'Введите слово для шифрования/дешифрования')
-#ent = Entry()
-#ext2 = Label()
-#btn = Button(text = 'Расшифровать!',command = cifer)
-#ext.pack()
-#ent.pack()
-#ext2.pack()
-#btn.pack()
-#root.mainloop()
